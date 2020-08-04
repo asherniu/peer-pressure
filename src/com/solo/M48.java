6d8eb2d255da8714cf678d30af76d95e6bf21d2a
ashashasher <asherniu@gmail.com>
2020-08-03 22:06:37 -0700

    Fk its monday

---
+++ src/com/solo/M48.java
@@ -0,0 +1,25 @@
+package com.solo;
+
+public class M48 {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i +1; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+}
